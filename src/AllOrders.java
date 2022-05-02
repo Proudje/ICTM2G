81f1024170cbc5f51807c24a613c7bf6cc3f8d0a
GisbertvanVeldhuisen <gisbertvveldhuisen@gmail.com>
2022-05-02 16:06:05 +0200

    all orders sql in Database.java

--- src/AllOrders.java
+++ src/AllOrders.java
@@ -3,41 +3,16 @@ import java.awt.*;
 import java.sql.*;
 
 public class AllOrders extends JPanel {
-    Connection connection;
-    PreparedStatement ps;
-    ResultSet rs = null;
-
     public AllOrders() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/nerdygadgets", "root", "root");
-            ps = connection.prepareStatement("SELECT `OrderID`, `OrderDate`, `CustomerName` FROM `orders` LEFT JOIN customers ON customers.CustomerID = orders.CustomerID LIMIT 10");
-            rs = ps.executeQuery();
-        } catch (Exception ex) {
-            System.out.println("Fouttt");
-        }
         setPreferredSize(new Dimension(900, 600));
 
-        String data[][] = new String[20][5];
-        int i = 0;
-        while (rs.next()) {
-            int id = rs.getInt("OrderID");
-            String name = rs.getString("CustomerName");
-            String age = rs.getString("OrderDate");
-            String deliverd = "yes";
-            String returned = "No";
-            data[i][0] = id + "";
-            data[i][1] = name;
-            data[i][2] = age;
-            data[i][3] = deliverd;
-            data[i][4] = returned;
-            i++;
-        }
 
-        String column[]={"OrderID","Customer name","Order date", "Deliverd", "Retour"};
-        JTable jt=new JTable(data,column);
-        jt.setBounds(30,40,200,300);
-        JScrollPane sp=new JScrollPane(jt);
+        String column[] = {"OrderID", "Customer name", "Order date", "Deliverd", "Retour"};
+        Database data = new Database();
+
+        JTable jt = new JTable(data.getOders(), column);
+        jt.setBounds(30, 40, 200, 300);
+        JScrollPane sp = new JScrollPane(jt);
         add(sp);
         setVisible(true);
     }
--- src/Database.java
+++ src/Database.java
@@ -1,4 +1,3 @@
-import javax.swing.*;
 import java.sql.*;
 
 public class Database {
@@ -25,11 +24,41 @@ public class Database {
             ResultSet result = ps.executeQuery();
             if (result.next()) {
                 return true;
+
             }
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
         return false;
+    }
+
+    public String[][] getOders() throws SQLException {
+        PreparedStatement ps;
+        ResultSet rs = null;
+        try {
+            Connection connection = getConnection();
+            ps = connection.prepareStatement("SELECT `OrderID`, `OrderDate`, `CustomerName` FROM `orders` LEFT JOIN customers ON customers.CustomerID = orders.CustomerID LIMIT 10");
+            rs = ps.executeQuery();
+        } catch (Exception ex) {
+            System.out.println("Fouttt");
+        }
+
+        String data[][] = new String[20][5];
+        int i = 0;
+        while (rs.next()) {
+            int id = rs.getInt("OrderID");
+            String name = rs.getString("CustomerName");
+            String age = rs.getString("OrderDate");
+            String deliverd = "yes";
+            String returned = "No";
+            data[i][0] = id + "";
+            data[i][1] = name;
+            data[i][2] = age;
+            data[i][3] = deliverd;
+            data[i][4] = returned;
+            i++;
+        }
+        return data;
     }
 
 }
