500ba4eb70c8653bb27ec454b95bd0ccab492d42
Proudje <verzaalb@gmail.com>
2022-05-11 14:22:18 +0200

    verder

---
+++ src/Customer.java
@@ -0,0 +1,74 @@
+public class Customer {
+    private int customerID;
+    private String name;
+    private String address;
+    private String postalcode;
+    private String phonenumber;
+    private String stateprovincename;
+    private String countryname;
+    private String cityname;
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostalcode() {
+        return postalcode;
+    }
+
+    public void setPostalcode(String postalcode) {
+        this.postalcode = postalcode;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getStateprovincename() {
+        return stateprovincename;
+    }
+
+    public void setStateprovincename(String stateprovincename) {
+        this.stateprovincename = stateprovincename;
+    }
+
+    public String getCountryname() {
+        return countryname;
+    }
+
+    public void setCountryname(String countryname) {
+        this.countryname = countryname;
+    }
+
+    public String getCityname() {
+        return cityname;
+    }
+
+    public void setCityname(String cityname) {
+        this.cityname = cityname;
+    }
+}
---
+++ src/EditOrder.java
@@ -0,0 +1,117 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+
+public class EditOrder extends JFrame implements ActionListener {
+    private static JLabel userLabel, addressLabel, postalcodeLabel, phonenumberLabel, countryLabel, country, stateProvinceLabel, stateProvice, cityNameLabel, cityName;
+    private static JTextField username, address, postalcode, phonenumber;
+    private static JButton save;
+
+
+
+    public EditOrder() throws SQLException {
+        Database data = new Database();
+        Customer customer = data.getCustomer(2);
+
+        setSize(800,500);
+        setVisible(true);
+        setTitle("Edit Order");
+        JPanel panel = new JPanel();
+        setLayout(new GridLayout(2,1));
+        JPanel tabel = new JPanel();
+        panel.setLayout(null);
+
+        tabel.setLayout(new FlowLayout());
+        add(panel);
+        add(tabel);
+
+        userLabel = new JLabel("Naam");
+        userLabel.setBounds(100, 10, 70, 20);
+        panel.add(userLabel);
+
+        username = new JTextField();
+        username.setBounds(100, 30, 250, 28);
+        panel.add(username);
+
+        countryLabel = new JLabel("Land: ");
+        countryLabel.setBounds(400, 10, 70, 20);
+        panel.add(countryLabel);
+
+        country = new JLabel(customer.getCountryname());
+        country.setBounds(500, 10, 200, 20);
+        panel.add(country);
+
+        stateProvinceLabel = new JLabel("Provincie: ");
+        stateProvinceLabel.setBounds(400, 30, 70, 20);
+        panel.add(stateProvinceLabel);
+
+        stateProvice = new JLabel(customer.getStateprovincename());
+        stateProvice.setBounds(500, 30, 200, 20);
+        panel.add(stateProvice);
+
+        cityNameLabel = new JLabel("Stad: ");
+        cityNameLabel.setBounds(400, 50, 70, 20);
+        panel.add(cityNameLabel);
+
+        cityName = new JLabel(customer.getCityname());
+        cityName.setBounds(500, 50, 200, 20);
+        panel.add(cityName);
+
+        addressLabel = new JLabel("Adres");
+        addressLabel.setBounds(100, 60, 70, 20);
+        panel.add(addressLabel);
+
+        address = new JTextField();
+        address.setBounds(100, 80, 250, 28);
+        panel.add(address);
+
+        postalcodeLabel = new JLabel("Postcode");
+        postalcodeLabel.setBounds(100, 110, 70, 20);
+        panel.add(postalcodeLabel);
+
+        postalcode = new JTextField();
+        postalcode.setBounds(100, 130, 250, 28);
+        panel.add(postalcode);
+
+        phonenumberLabel = new JLabel("Telefoonnummer");
+        phonenumberLabel.setBounds(100, 160, 70, 20);
+        panel.add(phonenumberLabel);
+
+        phonenumber = new JTextField();
+        phonenumber.setBounds(100, 180, 250, 28);
+        panel.add(phonenumber);
+
+        save = new JButton("Opslaan");
+        save.setBounds(650, 20, 90, 25);
+        save.addActionListener(this);
+        panel.add(save);
+
+        String column[] = {"OrderID", "Customer name", "Order date", "Deliverd", "Retour", "Edit"};
+
+        username.setText(customer.getName());
+        address.setText(customer.getAddress());
+        postalcode.setText(customer.getPostalcode());
+        phonenumber.setText(customer.getPhonenumber());
+
+        JTable jt = new JTable(data.getOders(), column);
+
+        jt.setBounds(100, 220, 400, 100);
+        jt.setAutoCreateRowSorter(true);
+        JScrollPane sp = new JScrollPane(jt);
+        tabel.add(sp);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Database data = new Database();
+        try {
+            boolean test = data.updateCustomer(2, username.getText(), phonenumber.getText(), address.getText(), postalcode.getText());
+            System.out.println(test);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
