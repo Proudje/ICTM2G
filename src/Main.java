2949fc9ecaa3f8e34f18643daf9e891c2eca16a9
Proudje <verzaalb@gmail.com>
2022-05-17 10:16:37 +0200

    verder

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        LoginFrame g = new LoginFrame();
+    public static void main(String[] args) throws SQLException {
+//        LoginFrame g = new LoginFrame();
+        Database data = new Database();
+        ArrayList<Location> routes = data.alogorithm();
+        StringBuilder url = new StringBuilder("https://www.google.com/maps/dir/");
+        for (Location r : routes) {
+            String gps = r.getLat() +","+ r.getLongg()+"/";
+            url.append(gps);
+//            System.out.println(r.getLat() +","+ r.getLongg()+"/");
+        }
+        System.out.println(url);
+
+
 
     }
 }
