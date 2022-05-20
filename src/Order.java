a51085f0d3e769ca59b0d0b7d6ea44c088ebb385
Proudje <verzaalb@gmail.com>
2022-05-20 16:17:07 +0200

    verder

--- src/Order.java
+++ src/Order.java
@@ -3,6 +3,6 @@ import java.util.ArrayList;
 public class Order {
     private int OrderID;
     private String OrderDate;
-    private ArrayList<Customer> Customer;
+    private Customer Customer;
     private ArrayList<Product> Product;
 }
