170ab440f441146c9bf913d72be070967dd29d52
Proudje <verzaalb@gmail.com>
2022-05-20 13:24:30 +0200

    verder

---
+++ src/Order.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Order {
+    private int OrderID;
+    private String OrderDate;
+    private ArrayList<Customer> Customer;
+    private ArrayList<Product> Product;
+}
