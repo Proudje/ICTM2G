e7fdde0acf4e9e3671b95ab2b145bacabdefe9bd
Proudje <verzaalb@gmail.com>
2022-05-24 13:26:39 +0200

    Query werkt

--- src/Order.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class Order {
-    private int OrderID;
-    private String OrderDate;
-    private ArrayList<Customer> Customer;
-    private ArrayList<Product> Product;
-}
