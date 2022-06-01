1c7bb7709d34b0e0dc1ed8b1555f11704ac27b3c
Proudje <verzaalb@gmail.com>
2022-06-01 15:49:29 +0200

    Query werkt

---
+++ src/WriteDataToExcel.java
@@ -0,0 +1,71 @@
+// Java program to write data in excel sheet using java code
+
+import java.io.File;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class WriteDataToExcel {
+
+    // any exceptions need to be caught
+    public WriteDataToExcel(ArrayList<Location> locations) throws IOException {
+
+        // workbook object
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+        // spreadsheet object
+        XSSFSheet spreadsheet
+                = workbook.createSheet(" Student Data ");
+
+        // creating a row object
+        XSSFRow row;
+
+        // This data needs to be written (Object[])
+        Map<String, Object[]> studentData
+                = new TreeMap<String, Object[]>();
+
+        studentData.put(
+                "1",
+                new Object[] { "Adres", "Naam", "Telefoonnummer" });
+        int i = 2;
+        for (Location location : locations) {
+            studentData.put(String.valueOf(i), new Object[] { location.getCustomer().getAddress(), location.getCustomer().getName(),location.getCustomer().getPhonenumber()});
+            i++;
+        }
+
+        Set<String> keyid = studentData.keySet();
+
+        int rowid = 0;
+
+        // writing the data into the sheets...
+
+        for (String key : keyid) {
+
+            row = spreadsheet.createRow(rowid++);
+            Object[] objectArr = studentData.get(key);
+            int cellid = 0;
+
+            for (Object obj : objectArr) {
+                Cell cell = row.createCell(cellid++);
+                cell.setCellValue((String)obj);
+            }
+        }
+
+        // .xlsx is the format for Excel Sheets...
+        // writing the workbook into the file...
+        FileOutputStream out = new FileOutputStream(
+                new File("/Users/boverzaal/GFGsheet.xlsx"));
+
+        workbook.write(out);
+        out.close();
+    }
+}
+
